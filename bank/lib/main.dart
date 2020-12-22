49f85f00db77275923d9e86d29817bf7dafa7ad2
Jucelania Silva <juselania98@outlook.com>
2020-12-21 22:19:21 -0300

    first commit

---
+++ bank/lib/main.dart
@@ -0,0 +1,50 @@
+import 'package:flutter/material.dart';
+
+void main() => runApp(MaterialApp(
+      home: Scaffold(
+        body: ListaTransferencias(),
+        appBar: AppBar(
+          title: Text('Transferências'),
+        ),
+        floatingActionButton: FloatingActionButton(
+          child: Icon(Icons.home),
+        ),
+      ),
+    ));
+
+class ListaTransferencias extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    // TODO: implement build
+    return Column(children: <Widget>[
+      ItemTransferencia(Transferencia(100, 1000)),
+      ItemTransferencia(Transferencia(200, 1001)),
+      ItemTransferencia(Transferencia(300, 2000)),
+    ]);
+  }
+}
+
+class ItemTransferencia extends StatelessWidget {
+  final Transferencia _transferencia;
+
+  ItemTransferencia(this._transferencia);
+
+  @override
+  Widget build(BuildContext context) {
+    // TODO: implement build
+    return Card(
+      child: ListTile(
+        leading: Icon(Icons.monetization_on),
+        title: Text(_transferencia.valor.toString()),
+        subtitle: Text(_transferencia.numeroConta.toString()),
+      ),
+    );
+  }
+}
+
+class Transferencia {
+  final double valor;
+  final int numeroConta;
+
+  Transferencia(this.valor, this.numeroConta);
+}
